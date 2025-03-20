2c78f85db3fa394eebcf4cee837ed5dbbf3b86a1
Monica-G30 <wangui16monica@gmail.com>
2025-03-20 12:50:57 +0300

    Created Dictionary.js and Dictionary.css files

---
+++ src/Dictionary.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyWord, setKeyWord] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyWord} definition`);
+  }
+  function handleKeyWordChange(event) {
+    setKeyWord(event.target.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeyWordChange} />
+      </form>
+    </div>
+  );
+}
