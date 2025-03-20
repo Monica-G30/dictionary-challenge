0e144aeb3ed35560a168210c4b172ed288990091
Monica-G30 <wangui16monica@gmail.com>
2025-03-20 13:33:15 +0300

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyWord} definition`);
+
+    let apiKey = "2ab48f58932e542cdo049f3057b190t0";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function handleKeyWordChange(event) {
     setKeyWord(event.target.value);
   }
