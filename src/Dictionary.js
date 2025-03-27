cb8bfb3393eddb3befa27a2874d8d88ff1a58345
Monica-G30 <wangui16monica@gmail.com>
2025-03-27 15:11:37 +0300

    intergrated the pexels api  to the code and d built photos.js fle

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,34 +1,70 @@
 import React, { useState } from "react";
-import "./Dictionary.css";
-import Results from "./Results";
 import axios from "axios";
+import Results from "./Results";
+import Photos from "./Photos";
+import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyWord, setKeyWord] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
-  function search(event) {
-    event.preventDefault();
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`;
-    console.log(apiUrl);
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+  function search() {
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "KEf23snemO1z1wYC9GHtqn5IAUVNiTjwvNez7jCL9rXwhMGVh4rHVBIM";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
-  function handleKeyWordChange(event) {
-    setKeyWord(event.target.value);
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
-  return (
-    <div>
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeyWordChange} />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>What word do you want to look up?</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="hint">
+            suggested words: sunset, wine, yoga, song...
+          </div>
+        </section>
+        <Results results={results} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
